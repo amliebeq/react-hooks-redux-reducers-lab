d264c456eb17a04e6775d4bbbcff7c8fca756788
alex lieberman <alex.lieberman182@gmail.com>
2023-06-22 09:09:25 -0700

    finished lab

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,12 @@
-export function manageFriends(state, action) {
-  // your code here
+export function manageFriends(state = {friends: []}, action) {
+  console.log(state.friends)
+  console.log(action.payload)
+  switch (action.type) {
+    default:
+      return state
+    case 'friends/add':
+      return {friends: [...state.friends, action.payload]}
+    case 'friends/remove':
+      return {friends: [...state.friends.filter((friend) => friend.id !== action.payload)]}          
+  }  
 }
